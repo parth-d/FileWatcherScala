c733b67d1280224e3ccb28b2a39b6b97dc04d6af
Parth <pdeshp8@uic.edu>
2021-11-20 13:22:25 -0600

    Kafka driver try

--- build.sbt
+++ build.sbt
@@ -6,8 +6,10 @@ scalaVersion := "2.13.7"
 
 val typesafeConfigVersion = "1.4.1"
 val AkkaVersion = "2.6.17"
+val kafkaVersion = "2.8.0"
 
 resolvers += "Typesafe Repository" at "https://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies += "com.typesafe" % "config" % typesafeConfigVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % AkkaVersion
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % kafkaVersion
--- src/main/scala/Actors/Extractor.scala
+++ src/main/scala/Actors/Extractor.scala
@@ -1,9 +1,11 @@
 package Actors
 
 import akka.actor.Actor
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 import java.io.File
 import java.nio.file.Files
+import java.util.Properties
 import scala.io.Source
 
 class Extractor extends Actor {
@@ -15,5 +17,29 @@ class Extractor extends Actor {
       val data = Files.lines(file.toPath)
       data.skip(lastReadLines(file.getName)).forEach(println)
       lastReadLines(file.getName) = BufferedSource.getLines.size
+      kafkaTry()
+  }
+
+  def kafkaTry(): Unit = {
+    val props: Properties = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("acks","all")
+
+    val producer = new KafkaProducer[String, String](props)
+    val topic = "test"
+    try {
+      val record = new ProducerRecord[String, String](topic, "0", "Hi Jeet")
+      val metadata = producer.send(record)
+      printf(s"sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d)\n",
+        record.key(), record.value(),
+        metadata.get().partition(),
+        metadata.get().offset())
+    }
+    catch {
+      case e: Exception => e.printStackTrace()
+    }
+    finally producer.close()
   }
 }
