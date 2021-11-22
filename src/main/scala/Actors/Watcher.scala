7d8da604597f26f05189fbbbae1f0ae9d0ac0e75
Parth <pdeshp8@uic.edu>
2021-11-22 17:37:59 -0600

    Dynamic path input via argument

--- src/main/scala/Actors/Watcher.scala
+++ src/main/scala/Actors/Watcher.scala
@@ -6,10 +6,10 @@ import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import java.io.File
 
 object Watcher {
-  def props(extractor: ActorRef): Props = Props(new Watcher(extractor))
+  def props(extractor: ActorRef, path: String): Props = Props(new Watcher(extractor, path))
 }
 
-class Watcher(extractor: ActorRef) extends Actor {
+class Watcher(extractor: ActorRef, path: String) extends Actor {
   override def receive: Receive = {
     case "watch" =>
       watch(extractor)
@@ -18,7 +18,7 @@ class Watcher(extractor: ActorRef) extends Actor {
 
   def watch(extractor: ActorRef): Unit = {
     //Specify the path here
-    val folder = new File("src/test/resources")
+    val folder = new File(path)
     val watcher = new FileWatcher(folder)
     watcher.addListener(new FileAdapter() {
       override def onModified(event: FileEvent): Unit = {
@@ -29,8 +29,9 @@ class Watcher(extractor: ActorRef) extends Actor {
 }
 
 object Main extends App {
+  val path = args(0)
   val system = ActorSystem("Watchers")
   val extractor = system.actorOf(Props[Extractor], name = "extractor")
-  val watcher = system.actorOf(Watcher.props(extractor), name = "watcher")
+  val watcher = system.actorOf(Watcher.props(extractor, path), name = "watcher")
   watcher ! "watch"
 }
