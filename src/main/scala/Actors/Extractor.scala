2abaf42770c84d9ed5baaab7136e5ca97ea0d635
Parth <pdeshp8@uic.edu>
2021-11-30 16:05:59 -0600

    - Kafka initial try
    TODO:
    - Kafka integration (Boilerplate ready)

--- src/main/scala/Actors/Extractor.scala
+++ src/main/scala/Actors/Extractor.scala
@@ -1,8 +1,10 @@
 package Actors
 
 import akka.actor.{Actor, Props}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 import java.io.File
+import java.util.Properties
 import scala.sys.process._
 
 object Extractor {
@@ -10,13 +12,13 @@ object Extractor {
 }
 
 class Extractor(file: File) extends Actor {
-//  val props: Properties = new Properties()
-//  props.put("bootstrap.servers", "localhost:9092")
-//  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-//  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-//  props.put("acks","all")
-//  val producer = new KafkaProducer[String, String](props)
-//  val topic = "test"
+  val props: Properties = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("acks","all")
+  val producer = new KafkaProducer[String, String](props)
+  val topic = "test"
 
   override def receive: Receive = {
     case str: String =>
@@ -29,16 +31,16 @@ class Extractor(file: File) extends Actor {
     val lastLine: Int  = boundaries.split(" ")(1).toInt
     val data: String = s"sed -n '$firstLine,$lastLine p' ${file.getAbsolutePath}".!!
     println(data)
-//    try {
-//      val record = new ProducerRecord(topic, "key",  data)
-//      val metadata = producer.send(record)
-//      printf(s"sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d)\n",
-//        record.key(), record.value(),
-//        metadata.get().partition(),
-//        metadata.get().offset())
-//    }
-//    catch {
-//      case e: Exception => e.printStackTrace()
-//    }
+    try {
+      val record = new ProducerRecord(topic, "key", data)
+      val metadata = producer.send(record)
+      printf(s"sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d)\n",
+        record.key(), record.value(),
+        metadata.get().partition(),
+        metadata.get().offset())
+    }
+    catch {
+      case e: Exception => e.printStackTrace()
+    }
   }
 }
