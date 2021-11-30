cb2341432a9c3c1d3e65653aa61abcfd811e2efd
Parth <pdeshp8@uic.edu>
2021-11-29 18:10:07 -0600

    - Intermediate logic changes - log if changes observed, fixed Extractor

--- src/main/scala/Actors/Extractor.scala
+++ src/main/scala/Actors/Extractor.scala
@@ -13,38 +13,39 @@ object Extractor {
 }
 
 class Extractor(file: File) extends Actor {
-  var lastReadLines: scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map[String, Int]()
-  var lineCounter = 0
-  val props: Properties = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("acks","all")
-  val producer = new KafkaProducer[String, String](props)
-  val topic = "test"
+//  var lastReadLines: scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map[String, Int]()
+//  var lineCounter = 0
+//  val props: Properties = new Properties()
+//  props.put("bootstrap.servers", "localhost:9092")
+//  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+//  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+//  props.put("acks","all")
+//  val producer = new KafkaProducer[String, String](props)
+//  val topic = "test"
 
   override def receive: Receive = {
-    case file: File =>
-      if (!lastReadLines.contains(file.getName)) lastReadLines += (file.getName -> 0)
-      val BufferedSource = Source.fromFile(file)
-      val data = Files.lines(file.toPath)
-      data.skip(lastReadLines(file.getName)).forEach(kafkaTry(_))
-      lastReadLines(file.getName) = BufferedSource.getLines.size
-//      kafkaTry()
+    case str: String =>
+                  println("Parth:\t" + self.path.name + " received: " + str)
+//      if (!lastReadLines.contains(file.getName)) lastReadLines += (file.getName -> 0)
+//      val BufferedSource = Source.fromFile(file)
+//      val data = Files.lines(file.toPath)
+//      data.skip(lastReadLines(file.getName)).forEach(kafkaTry(_))
+//      lastReadLines(file.getName) = BufferedSource.getLines.size
+////      kafkaTry()
   }
 
-  def kafkaTry(data: String): Unit = {
-    try {
-      val record = new ProducerRecord(topic, "key",  data)
-      val metadata = producer.send(record)
-      printf(s"sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d)\n",
-        record.key(), record.value(),
-        metadata.get().partition(),
-        metadata.get().offset())
-    }
-    catch {
-      case e: Exception => e.printStackTrace()
-    }
-//    finally producer.close()
-  }
+//  def kafkaTry(data: String): Unit = {
+//    try {
+//      val record = new ProducerRecord(topic, "key",  data)
+//      val metadata = producer.send(record)
+//      printf(s"sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d)\n",
+//        record.key(), record.value(),
+//        metadata.get().partition(),
+//        metadata.get().offset())
+//    }
+//    catch {
+//      case e: Exception => e.printStackTrace()
+//    }
+////    finally producer.close()
+//  }
 }
