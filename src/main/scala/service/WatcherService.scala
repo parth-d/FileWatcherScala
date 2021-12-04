b3444d8a65b29d1c4a648a9f945bf2b0877f126d
aditya <adityakanthale@gmail.com>
2021-12-03 21:54:51 -0600

    optimize with tailrec

--- src/main/scala/service/WatcherService.scala
+++ src/main/scala/service/WatcherService.scala
@@ -5,6 +5,7 @@ import constants.ShellCommands
 
 import java.io.File
 import java.util.logging.{Level, Logger}
+import scala.annotation.tailrec
 import sys.process._
 
 
@@ -12,12 +13,19 @@ class WatcherService {
 
   val logger: Logger = Logger.getLogger(this.getClass.getName)
 
+
   def watch(extractor: ActorRef, file: File, lastReadLine: Int): Unit = {
-    val newLine: Int = (ShellCommands.wc + file.getAbsolutePath).!!.split(" ")(0).toInt
-    if (newLine != lastReadLine) {
-      logger.log(Level.FINE, "Changes observed, sending message to extractor " + extractor.path.name)
-      extractor ! lastReadLine + " " + newLine
+
+    @tailrec
+    def _watch (extractor: ActorRef, file: File, lastReadLine: Int): Unit = {
+      val newLine: Int = (ShellCommands.wc + file.getAbsolutePath).!!.split(" ")(0).toInt
+      if (newLine != lastReadLine) {
+        logger.log(Level.FINE, "Changes observed, sending message to extractor " + extractor.path.name)
+        extractor ! lastReadLine + " " + newLine
+      }
+      _watch(extractor, file, newLine) 
     }
-    watch(extractor, file, newLine)
+
+    _watch(extractor, file, lastReadLine)
   }
 }
